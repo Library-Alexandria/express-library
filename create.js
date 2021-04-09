0d686708137fa8e098829c49ff9822f2f13bba69
jtreeves <jackson.t.reeves@gmail.com>
2021-04-09 16:08:54 -0400

    [create] Grab all results by hardcoding API calls for each page of results

--- create.js
+++ create.js
@@ -5,36 +5,108 @@ const url = 'http://gutendex.com/books?languages=en&copyright=false&author_year_
 
 async function getBooks() {
     let bookJSON = ''
-
-    async function getResults(requestUrl) {
-        console.log(`RESQUEST URL: ${requestUrl}`)
-        let books = await axios.get(requestUrl)
-        for (const book of books.data.results) {
-            let subjects = book.subjects
-            let subjectsString = ''
-            for (const subject of subjects) {
-                subjectsString += `${subject}, `
-            }
-            let formats = Object.keys(book.formats)
-            let formatText = ''
-            for (const format of formats) {
-                if (format.startsWith('text/html')) {
-                    formatText = format
-                }
-            }
-            let trimmedSubjects = subjectsString.slice(0, -2)
-            bookJSON += `{\n    title: "${book.title.replace(/"/g, '')}",\n    author: "${book.authors[0].name}",\n    subjects: "${trimmedSubjects}",\n    image: "${book.formats['image/jpeg']}",\n    text: "${book.formats[formatText]}",\n    createdAt: new Date(),\n    updatedAt: new Date()\n},\n`
+    const books1 = await axios.get(url)
+    const books2 = await axios.get(url + '&page=2')
+    const books3 = await axios.get(url + '&page=3')
+    const books4 = await axios.get(url + '&page=4')
+    const books5 = await axios.get(url + '&page=5')
+    const books6 = await axios.get(url + '&page=6')
+    for (const book of books1.data.results) {
+        let subjects = book.subjects
+        let subjectsString = ''
+        for (const subject of subjects) {
+            subjectsString += `${subject}, `
         }
-        // console.log(`INITIAL SLICES: ${bookJSON.slice(0, 50)}`)
-        console.log(`BOOKS.DATA.NEXT: ${books.data.next}`)
-        if (books.data.next) {
-            getResults(books.data.next)
+        let formats = Object.keys(book.formats)
+        let formatText = ''
+        for (const format of formats) {
+            if (format.startsWith('text/html')) {
+                formatText = format
+            }
         }
-        // console.log(`FINAL: ${bookJSON}`)
+        let trimmedSubjects = subjectsString.slice(0, -2)
+        bookJSON += `{\n    title: "${book.title.replace(/"/g, '')}",\n    author: "${book.authors[0].name}",\n    subjects: "${trimmedSubjects}",\n    image: "${book.formats['image/jpeg']}",\n    text: "${book.formats[formatText]}",\n    createdAt: new Date(),\n    updatedAt: new Date()\n},\n`
     }
-
-    getResults(url)
-
+    for (const book of books2.data.results) {
+        let subjects = book.subjects
+        let subjectsString = ''
+        for (const subject of subjects) {
+            subjectsString += `${subject}, `
+        }
+        let formats = Object.keys(book.formats)
+        let formatText = ''
+        for (const format of formats) {
+            if (format.startsWith('text/html')) {
+                formatText = format
+            }
+        }
+        let trimmedSubjects = subjectsString.slice(0, -2)
+        bookJSON += `{\n    title: "${book.title.replace(/"/g, '')}",\n    author: "${book.authors[0].name}",\n    subjects: "${trimmedSubjects}",\n    image: "${book.formats['image/jpeg']}",\n    text: "${book.formats[formatText]}",\n    createdAt: new Date(),\n    updatedAt: new Date()\n},\n`
+    }
+    for (const book of books3.data.results) {
+        let subjects = book.subjects
+        let subjectsString = ''
+        for (const subject of subjects) {
+            subjectsString += `${subject}, `
+        }
+        let formats = Object.keys(book.formats)
+        let formatText = ''
+        for (const format of formats) {
+            if (format.startsWith('text/html')) {
+                formatText = format
+            }
+        }
+        let trimmedSubjects = subjectsString.slice(0, -2)
+        bookJSON += `{\n    title: "${book.title.replace(/"/g, '')}",\n    author: "${book.authors[0].name}",\n    subjects: "${trimmedSubjects}",\n    image: "${book.formats['image/jpeg']}",\n    text: "${book.formats[formatText]}",\n    createdAt: new Date(),\n    updatedAt: new Date()\n},\n`
+    }
+    for (const book of books4.data.results) {
+        let subjects = book.subjects
+        let subjectsString = ''
+        for (const subject of subjects) {
+            subjectsString += `${subject}, `
+        }
+        let formats = Object.keys(book.formats)
+        let formatText = ''
+        for (const format of formats) {
+            if (format.startsWith('text/html')) {
+                formatText = format
+            }
+        }
+        let trimmedSubjects = subjectsString.slice(0, -2)
+        bookJSON += `{\n    title: "${book.title.replace(/"/g, '')}",\n    author: "${book.authors[0].name}",\n    subjects: "${trimmedSubjects}",\n    image: "${book.formats['image/jpeg']}",\n    text: "${book.formats[formatText]}",\n    createdAt: new Date(),\n    updatedAt: new Date()\n},\n`
+    }
+    for (const book of books5.data.results) {
+        let subjects = book.subjects
+        let subjectsString = ''
+        for (const subject of subjects) {
+            subjectsString += `${subject}, `
+        }
+        let formats = Object.keys(book.formats)
+        let formatText = ''
+        for (const format of formats) {
+            if (format.startsWith('text/html')) {
+                formatText = format
+            }
+        }
+        let trimmedSubjects = subjectsString.slice(0, -2)
+        bookJSON += `{\n    title: "${book.title.replace(/"/g, '')}",\n    author: "${book.authors[0].name}",\n    subjects: "${trimmedSubjects}",\n    image: "${book.formats['image/jpeg']}",\n    text: "${book.formats[formatText]}",\n    createdAt: new Date(),\n    updatedAt: new Date()\n},\n`
+    }
+    for (const book of books6.data.results) {
+        let subjects = book.subjects
+        let subjectsString = ''
+        for (const subject of subjects) {
+            subjectsString += `${subject}, `
+        }
+        let formats = Object.keys(book.formats)
+        let formatText = ''
+        for (const format of formats) {
+            if (format.startsWith('text/html')) {
+                formatText = format
+            }
+        }
+        let trimmedSubjects = subjectsString.slice(0, -2)
+        bookJSON += `{\n    title: "${book.title.replace(/"/g, '')}",\n    author: "${book.authors[0].name}",\n    subjects: "${trimmedSubjects}",\n    image: "${book.formats['image/jpeg']}",\n    text: "${book.formats[formatText]}",\n    createdAt: new Date(),\n    updatedAt: new Date()\n},\n`
+    }
     const trimmedObjects = bookJSON.slice(0, -2)
     const arrayWrapped = `[${trimmedObjects}]`
     fs.writeFile('seeder.js', arrayWrapped, (err) => {
