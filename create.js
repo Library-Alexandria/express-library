37678f5bd17f0e2094d577daba944fbcbb51e9a3
jtreeves <jackson.t.reeves@gmail.com>
2021-04-09 13:57:18 -0400

    [create] Move fs commands to write to seeder file out of the server file and into its own file

---
+++ create.js
@@ -0,0 +1,27 @@
+const axios = require('axios')
+const fs = require('fs')
+
+const url = 'http://gutendex.com/books?languages=en&copyright=false&author_year_end=-50'
+
+async function getBooks() {
+    const books = await axios.get(url)
+    let bookJSON = ''
+    for (const book of books.data.results) {
+        let subjects = book.subjects
+        let subjectsString = ''
+        for (const subject of subjects) {
+            subjectsString += `${subject}, `
+        }
+        let trimmedSubjects = subjectsString.slice(0, -2)
+        bookJSON += `{\n    title: "${book.title}",\n    author: "${book.authors[0].name}",\n    subjects: "${trimmedSubjects}",\n    image: "${book.formats['image/jpeg']}",\n    createdAt: new Date(),\n    updatedAt: new Date()\n},\n`
+    }
+    const trimmedObjects = bookJSON.slice(0, -2)
+    const arrayWrapped = `[${trimmedObjects}]`
+    fs.writeFile('seeder.js', arrayWrapped, (err) => {
+        if (err) {
+            console.log(err)
+        }
+    })
+}
+
+getBooks()
--- server.js
+++ server.js
@@ -1,27 +1,6 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const axios = require('axios')
-const fs = require('fs')
-
-const url = 'http://gutendex.com/books?languages=en&copyright=false&author_year_end=-50'
-async function getBooks() {
-    const books = await axios.get(url)
-    let bookJSON = ''
-    for (const book of books.data.results) {
-        bookJSON += `{\n    title: "${book.title}",\n    author: "${book.authors[0].name}",\n    createdAt: new Date(),\n    updatedAt: new Date()\n},\n`
-    }
-    const trimmedObjects = bookJSON.slice(0, -2)
-    const arrayWrapped = `[${trimmedObjects}]`
-    fs.writeFile('seeder.js', arrayWrapped, (err) => {
-        if (err) {
-            console.log(err)
-        }
-    })
-}
-
-getBooks()
-
 
 app.get('/', (req, res) => {
     res.send('Test hello')
