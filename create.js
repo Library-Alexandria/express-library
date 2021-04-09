dabdf01b38f9f90bfc8cd7ab3804d1245fdda3fe
jtreeves <jackson.t.reeves@gmail.com>
2021-04-09 15:33:36 -0400

    [create] Attempt to get all the results with a for loop and a conditional

--- create.js
+++ create.js
@@ -5,26 +5,11 @@ const url = 'http://gutendex.com/books?languages=en&copyright=false&author_year_
 
 async function getBooks() {
     let bookJSON = ''
-    const books = await axios.get(url)
-    for (const book of books.data.results) {
-        let subjects = book.subjects
-        let subjectsString = ''
-        for (const subject of subjects) {
-            subjectsString += `${subject}, `
-        }
-        let formats = Object.keys(book.formats)
-        let formatText = ''
-        for (const format of formats) {
-            if (format.startsWith('text/html')) {
-                formatText = format
-            }
-        }
-        let trimmedSubjects = subjectsString.slice(0, -2)
-        bookJSON += `{\n    title: "${book.title.replace(/"/g, '')}",\n    author: "${book.authors[0].name}",\n    subjects: "${trimmedSubjects}",\n    image: "${book.formats['image/jpeg']}",\n    text: "${book.formats[formatText]}",\n    createdAt: new Date(),\n    updatedAt: new Date()\n},\n`
-    }
-    if (books.data.next) {
-        const moreBooks = await axios.get(books.data.next)
-        for (const book of moreBooks.data.results) {
+
+    async function getResults(requestUrl) {
+        console.log(`RESQUEST URL: ${requestUrl}`)
+        let books = await axios.get(requestUrl)
+        for (const book of books.data.results) {
             let subjects = book.subjects
             let subjectsString = ''
             for (const subject of subjects) {
@@ -40,7 +25,16 @@ async function getBooks() {
             let trimmedSubjects = subjectsString.slice(0, -2)
             bookJSON += `{\n    title: "${book.title.replace(/"/g, '')}",\n    author: "${book.authors[0].name}",\n    subjects: "${trimmedSubjects}",\n    image: "${book.formats['image/jpeg']}",\n    text: "${book.formats[formatText]}",\n    createdAt: new Date(),\n    updatedAt: new Date()\n},\n`
         }
+        // console.log(`INITIAL SLICES: ${bookJSON.slice(0, 50)}`)
+        console.log(`BOOKS.DATA.NEXT: ${books.data.next}`)
+        if (books.data.next) {
+            getResults(books.data.next)
+        }
+        // console.log(`FINAL: ${bookJSON}`)
     }
+
+    getResults(url)
+
     const trimmedObjects = bookJSON.slice(0, -2)
     const arrayWrapped = `[${trimmedObjects}]`
     fs.writeFile('seeder.js', arrayWrapped, (err) => {
