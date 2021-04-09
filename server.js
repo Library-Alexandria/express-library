341891918263362c1c57e4119da43bdb3315962f
nicole <nikkihmltn1@yahoo.com>
2021-04-09 09:56:29 -0700

    set up js files

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//test home route
 app.get('/', (req, res) => {
     res.send('Test hello')
 })
 
+//controllers
+app.use('/users', require('/users'))
+app.use('/books', require('/books'))
+app.use('/favorites', require('/favorites'))
+
+//port listener
 app.listen(port, () => {
     console.log('server up and running!')
 })
