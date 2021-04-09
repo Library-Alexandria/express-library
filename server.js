8b24a731e78cea316cc005e09a58265d58cfcf2b
jtreeves <jackson.t.reeves@gmail.com>
2021-04-09 13:18:35 -0400

    [seeder] Use fs to write data from API call to a file

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const axios = require('axios')
+const fs = require('fs')
+
+const url = 'http://gutendex.com/books?languages=en&copyright=false&author_year_end=-50'
+async function getBooks() {
+    const books = await axios.get(url)
+    let bookJSON = ''
+    for (let i = 0; i < books.data.results.length; i++) {
+        bookJSON += `"${i}": { "title": "${books.data.results[i].title}" },\n`
+    }
+    const allBooks = `{${bookJSON.slice(0, -2)}}`
+    fs.writeFile('seeder.json', allBooks, (err) => {
+        if (err) {
+            console.log(err)
+        }
+    })
+}
+
+getBooks()
+
 
 app.get('/', (req, res) => {
     res.send('Test hello')
