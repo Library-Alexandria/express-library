94211e572ae0c7cfcf4dbe6b10f2d0fbcfaef8ef
jtreeves <jackson.t.reeves@gmail.com>
2021-04-10 16:43:55 -0400

    [seeders] Add fake books to seeder file for all books

--- seeders/filter.js
+++ seeders/filter.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const books = require('./data')
+const fakes = require('./fakes')
 
 books.sort((first, second) => {
     if (first.title < second.title) {
@@ -18,6 +19,10 @@ for (let i = 0; i < books.length - 1; i++) {
 let stringBooks = ''
 
 for (const book of books) {
+    stringBooks += `            {\n                title: "${book.title}",\n                author: "${book.author}",\n                subjects: "${book.subjects}",\n                image: "${book.image}",\n                text: "${book.text}",\n                createdAt: new Date(),\n                updatedAt: new Date()\n            },\n`
+}
+
+for (const book of fakes) {
     stringBooks += `            {\n                title: "${book.title}",\n                author: "${book.author}",\n                subjects: "${book.subjects}",\n                image: "${book.image}",\n                text: "${book.text}",\n                createdAt: new Date(),\n                updatedAt: new Date()\n            },\n`
 }
 
