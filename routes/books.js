3a9521d015d4778f645a3a15c38ec20abb25f7da
Your Name <you@example.com>
2021-04-10 11:48:52 -0400

    [books.js] fixed get route

--- routes/books.js
+++ routes/books.js
@@ -1,23 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../models");
-const user = require("../models/user");
-
-router.post("/post", async(req, res) => {
-    const book = await User.create({ 
-        id:1,
-        name:"Necronomicon",
-        author:"???",
-        subject:"Monsters",
-        description:"???",
-        image:"???",
-        text:"???"
-    })
-
-})
+const models = require("../models");
+// const user = require("../models/user");
 
 router.get("/:author", async (req, res) => {
-    db.findAll({
+    models.User.findAll({
         where: {
             author: req.params.author
         }
